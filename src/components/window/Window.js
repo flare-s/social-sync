c34500b2cc6bb3a7a7788de436fb48ef7c6199bd
flare-s <webflarez@gmail.com>
2022-11-26 12:05:13 +0000

    Basic styles to the windows

--- src/components/window/Window.js
+++ src/components/window/Window.js
@@ -1,4 +1,5 @@
 import windowStyles from "./Window.module.scss";
+import LockStyles from "./Lock.module.scss";
 
 const Window = ({ windowObj, setWindows }) => {
   const handleClick = (id) => {
@@ -23,7 +24,12 @@ const Window = ({ windowObj, setWindows }) => {
       }`}
       onClick={() => handleClick(windowObj.id)}
     >
-      <div>{windowObj.day}</div>
+      <div className={windowStyles.day}>
+        <span>{windowObj.day}</span>
+      </div>
+      <div className={LockStyles.container}>
+        <div className={LockStyles.lock}></div>
+      </div>
       <div>{windowObj.text}</div>
     </li>
   );
