5c4278d2dc01c1815efe29daa3b61a8aa2e76520
flare-s <webflarez@gmail.com>
2022-11-27 12:32:36 +0000

    Added a close button to the window content

--- src/components/window/Window.js
+++ src/components/window/Window.js
@@ -1,8 +1,10 @@
 import windowStyles from "./Window.module.scss";
 import LockStyles from "./Lock.module.scss";
+import React, { useRef } from "react";
 
 const Window = ({ windowObj, setWindows, selectedDayDate }) => {
-  const handleClick = (id) => {
+  const video = useRef();
+  const openWindow = (id) => {
     if (selectedDayDate < windowObj.day) return;
     setWindows((prevState) => {
       const newWindows = prevState.map((window) => {
@@ -17,6 +19,24 @@ const Window = ({ windowObj, setWindows, selectedDayDate }) => {
       return newWindows;
     });
   };
+  const closeWindow = (event, id) => {
+    if (event.target.classList.contains("video-btn")) {
+      video.current.pause();
+      video.current.currentTime = 0;
+    }
+    setWindows((prevState) => {
+      const newWindows = prevState.map((window) => {
+        return window.id === id
+          ? {
+              ...window,
+              open: false,
+            }
+          : window;
+      });
+      localStorage.setItem("windows", JSON.stringify(newWindows));
+      return newWindows;
+    });
+  };
   return (
     <li
       className={`${windowStyles.window} ${
@@ -24,22 +44,44 @@ const Window = ({ windowObj, setWindows, selectedDayDate }) => {
           ? windowStyles.active
           : ""
       }`}
-      onClick={() => handleClick(windowObj.id)}
     >
       {selectedDayDate < windowObj.day && (
         <div className={LockStyles.container}>
           <div className={LockStyles.lock}>{windowObj.day}</div>
         </div>
       )}
-      <div className={windowStyles.day}>
+      <div
+        className={windowStyles.day}
+        onClick={() => openWindow(windowObj.id)}
+      >
         <span>{windowObj.day}</span>
       </div>
       <div>
-        {windowObj.type === "image" && <img src={windowObj.source} alt="" />}
+        {windowObj.type === "image" && (
+          <div>
+            <button
+              className={windowStyles["close-window"]}
+              aria-label="Close window"
+              onClick={(e) => closeWindow(e, windowObj.id)}
+            >
+              <span aria-hidden>X</span>
+            </button>
+            <img src={windowObj.source} alt="" />
+          </div>
+        )}
         {windowObj.type === "video" && (
-          <video src={windowObj.source} width="100%" controls>
-            sorry, your browser doesn't support embedded video
-          </video>
+          <div>
+            <button
+              className={`${windowStyles["close-window"]} video-btn`}
+              aria-label="Close window"
+              onClick={(e) => closeWindow(e, windowObj.id)}
+            >
+              <span aria-hidden>X</span>
+            </button>
+            <video src={windowObj.source} width="100%" controls ref={video}>
+              sorry, your browser doesn't support embedded video
+            </video>
+          </div>
         )}
       </div>
     </li>
